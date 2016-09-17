831def9798b84618b095400e6f2cda4f29a0d01b
KC7 <kartikchugh.kc@gmail.com>
2016-09-16 21:42:18 -0400

    Broad Indev Commit
    Frame:
    - Constructor accepts Activity and title parameters (should be specified
    from subclass)
    Activity:
    - Constructor accepts width, height, framerate parameters (specified
    from subclass)
    General:
    - Cleanup, reformat, modifications

--- src/net/kc7/activityframe/Activity.java
+++ src/net/kc7/activityframe/Activity.java
@@ -14,78 +14,72 @@ public class Activity extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	
-	private final int ACTIVITY_WIDTH = 100;
-	private final int ACTIVITY_HEIGHT = 100;
-	private final int ACTIVITY_FRAMERATE = 1000;
+	private final int ACTIVITY_WIDTH;
+	private final int ACTIVITY_HEIGHT;
+	private final int ACTIVITY_FRAMERATE;
 	
 	private boolean active = false;
 	
-	private static final int UP = KeyEvent.VK_UP;
-	private static final int DOWN = KeyEvent.VK_DOWN;
-	private static final int LEFT = KeyEvent.VK_LEFT;
-	private static final int RIGHT = KeyEvent.VK_RIGHT;
+	protected static final int UP = KeyEvent.VK_UP;
+	protected static final int DOWN = KeyEvent.VK_DOWN;
+	protected static final int LEFT = KeyEvent.VK_LEFT;
+	protected static final int RIGHT = KeyEvent.VK_RIGHT;
 	
-	public Activity() {
+	public Activity(int width, int height, int framerate) {	
+		ACTIVITY_WIDTH = width;
+		ACTIVITY_HEIGHT = height;
+		ACTIVITY_FRAMERATE = framerate;
 		
 		setFocusable(true);
 		setPreferredSize(new Dimension(ACTIVITY_WIDTH, ACTIVITY_HEIGHT));
 		addKeyListener(new KeyHandler());
 		
 		loadResources();
-		startActivity();
-		
+		startActivity();	
 	}
 	
-	protected boolean loadResources() {
+	private boolean loadResources() {
 		return true;
 	}
 	
-	protected boolean processInfo() {
-		return true;
-	}
-	
-	protected boolean drawView(Graphics g) {
-		return true;
-	}
-	
-	protected boolean drawVoid(Graphics g) {
-		return true;
-	}
-	
-	protected boolean handleKey(int key) {
-		return true;
-	}
-	
-	private void startActivity() {
-		
+	private final void startActivity() {
 		active = true;
 		
 		Timer timer = new Timer(ACTIVITY_FRAMERATE, this);
 		timer.start();
 	}
 	
+	private boolean processInfo() {
+		return true;
+	}
+	
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		if (active) {
-			processInfo();
-		}
-		
+	public final void actionPerformed(ActionEvent e) {
+		if (active) processInfo();
 		repaint();
 	}
 	
 	@Override
-	public void paintComponent(Graphics g) {
+	public final void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		if (active) {
-			drawView(g);
-		} else {
-			drawVoid(g);
-		}
+		if (active) drawView(g);
+		else drawVoid(g);
 	}
 	
-	private class KeyHandler extends KeyAdapter {
+	private boolean drawView(Graphics g) {
+		return true;
+	}
+	
+	private boolean drawVoid(Graphics g) {
+		return true;
+	}
+	
+	private boolean handleKey(int key) {
+		return true;
+	}
+	
+	private final class KeyHandler extends KeyAdapter {
 		
 		@Override
 		public void keyPressed(KeyEvent e) {
