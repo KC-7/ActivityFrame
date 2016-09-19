204c59b55eebc73fd86a99d78313de32f84f8a0f
KC7 <kartikchugh.kc@gmail.com>
2016-09-19 18:46:42 -0400

    Revert "Revert "Major Update -- v0.9""
    This reverts commit 60bb4632bc2ad28d01b4c57aee907e2fa404a4c2.

--- src/net/kc7/activityframe/Activity.java
+++ src/net/kc7/activityframe/Activity.java
@@ -10,15 +10,14 @@ import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Activity extends JPanel implements ActionListener {
+public abstract class Activity extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	
-	private final int ACTIVITY_WIDTH;
-	private final int ACTIVITY_HEIGHT;
 	private final int ACTIVITY_FRAMERATE;
 	
-	private boolean active = false;
+	protected boolean active = false;
+	protected Timer timer;
 	
 	protected static final int UP = KeyEvent.VK_UP;
 	protected static final int DOWN = KeyEvent.VK_DOWN;
@@ -26,36 +25,37 @@ public class Activity extends JPanel implements ActionListener {
 	protected static final int RIGHT = KeyEvent.VK_RIGHT;
 	
 	public Activity(int width, int height, int framerate) {	
-		ACTIVITY_WIDTH = width;
-		ACTIVITY_HEIGHT = height;
 		ACTIVITY_FRAMERATE = framerate;
 		
 		setFocusable(true);
-		setPreferredSize(new Dimension(ACTIVITY_WIDTH, ACTIVITY_HEIGHT));
+		setPreferredSize(new Dimension(width, height));
 		addKeyListener(new KeyHandler());
 		
 		loadResources();
 		startActivity();	
 	}
 	
-	// OVERRIDE -- Loads images, audio, and other assets
-	protected boolean loadResources() {
-		return true;
-	}
+	// Loads images, audio, and other assets
+	protected abstract void loadResources();
 	
-	// DEFAULT -- Kicks off the activity
-	protected boolean startActivity() {
+	// Kicks off the activity
+	protected void startActivity() {
 		active = true;
-		Timer timer = new Timer(ACTIVITY_FRAMERATE, this);
+		timer = new Timer(ACTIVITY_FRAMERATE, this);
 		timer.start();
-		
-		return true;
 	}
 	
-	// OVERRIDE -- Handle data and execute tasks every frame
-	private boolean processInfo() {
-		return true;
-	}
+	// Handle data and execute tasks every frame
+	protected abstract void processInfo();
+	
+	// Draws the View every active frame
+	protected abstract void drawView(Graphics g);
+	
+	// Draws the Void for an inactive frame
+	protected abstract void drawVoid(Graphics g);
+	
+	// Handles key values
+	protected abstract void handleKey(int key);
 	
 	@Override
 	public final void actionPerformed(ActionEvent e) {
@@ -64,33 +64,18 @@ public class Activity extends JPanel implements ActionListener {
 	}
 	
 	@Override
-	public final void paintComponent(Graphics g) {
+	protected final void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
 		if (active) drawView(g);
 		else drawVoid(g);
 	}
 	
-	// OVERRIDE -- Draws the View every active frame
-	protected boolean drawView(Graphics g) {
-		return true;
-	}
-	
-	protected boolean drawVoid(Graphics g) {
-		return true;
-	}
-	
-	private boolean handleKey(int key) {
-		return true;
-	}
-	
 	private final class KeyHandler extends KeyAdapter {
 		
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
 			handleKey(e.getKeyCode());
-			
 		}
 	}
 }
